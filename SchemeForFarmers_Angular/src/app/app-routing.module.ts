6006e20650fe0b566718d6b03cf0d563b29d4c8f
rvp39 <vipulk3009@gmail.com>
2022-07-30 15:21:47 +0530

    angular component for farmer registration ,bidder registration and login created

--- SchemeForFarmers_Angular/src/app/app-routing.module.ts
+++ SchemeForFarmers_Angular/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import {FarmerRegistrationComponent} from './farmer-registration/farmer-registration.component'
+import {BidderRegistrationComponent} from './bidder-registration/bidder-registration.component'
+import  {LoginComponent} from './login/login.component'
+const routes: Routes = [
+  {path:'farmerSignup',component:FarmerRegistrationComponent},
+  {path:'bidderSignup',component:BidderRegistrationComponent},
+  {path:'logIn',component:LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
